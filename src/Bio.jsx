80396f8de47086e63180ff551201c7d23d356a10
ecomeel <ecomeel@gmail.com>
2023-09-22 16:46:18 +0300

    sex

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -14,6 +14,7 @@ export default function Bio() {
         name: "",
         surname: "",
         age: 0,
+        sex: "male",
     });
 
     const [isError, setError] = useState(false);
@@ -84,10 +85,41 @@ export default function Bio() {
                 Set age
             </button>
 
+            <p>
+                Select sex:
+                <label>
+                    <input
+                        type="radio"
+                        name="sex"
+                        value="male"
+                        checked={form.sex === "male"}
+                        onChange={handleInputChange}
+                    />
+                    Male
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="sex"
+                        value="female"
+                        checked={form.sex === "female"}
+                        onChange={handleInputChange}
+                    />
+                    Female
+                </label>
+            </p>
+            <button
+                onClick={() => {
+                    handleBtnClick("sex");
+                }}
+            >
+                Set sex
+            </button>
+
             <p>Name: {person.name}</p>
             <p>Surname: {person.surname}</p>
             <p>Age: {person.age}</p>
-            <p>Sex: {person.male}</p>
+            <p>Sex: {person.sex}</p>
             <p>Interests</p>
             <div>
                 {person.interests.map((interest) => (
