d3e3171162ad5d3fe3cef48dc35cc796515f5a80
ecomeel <ecomeel@gmail.com>
2023-11-02 16:03:12 +0300

    loading data from server

--- src/App2.jsx
+++ src/App2.jsx
@@ -8,21 +8,41 @@ import
 export default function App2() {
 
     const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     
     console.log('component rendered')
 
     useEffect(() => {
+        setIsLoading(true)
+
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(data => {
             console.log(data)
             setUsers(data)
+            setIsLoading(false)
         })
     }, []);
 
+    // const [count, setCount] = useState(0);
+
+    // console.log('components rendered')
+
+    // useEffect(() => {
+    //     console.log('useEffect call')
+    //     localStorage.setItem('count', count)
+    // }, [count]);
     return (
         <div>
-            {JSON.stringify(users)}
+            {isLoading ? 'loading' : JSON.stringify(users)}
         </div>
+        // <div>
+        //     {count}
+        //     <div>
+        //         <button onClick={() => setCount(count + 1)}>
+        //             Counter ++
+        //         </button>
+        //     </div>
+        // </div>
     )
 }
